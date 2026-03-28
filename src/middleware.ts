d8d79a5976d65b6286c1eac43cdd439c7cdf9999
oribe-inamasu <inamasu@oribe-design.net>
2026-03-28 11:57:27 +0900

    Allow webhook to bypass authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,6 @@ export const { auth } = NextAuth(authConfig)
 export default auth;
 
 export const config = {
-    // 保護すべきルートを指定（api/auth, 静的ファイルなどは除外）
-    matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+    // 保護すべきルートを指定（api/auth, api/webhook, 静的ファイルなどは除外）
+    matcher: ["/((?!api/auth|api/webhook|_next/static|_next/image|favicon.ico).*)"],
 }
